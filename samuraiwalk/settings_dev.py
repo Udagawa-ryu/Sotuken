fda199e7ce7b9a3913e9b359becb04e5e3b7a6d0
Udagawa-ryu <93694931+Udagawa-ryu@users.noreply.github.com>
2022-11-16 09:22:36 +0900

    'second'

---
+++ samuraiwalk/settings_dev.py
@@ -0,0 +1,41 @@
+from .settings_common import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# ロギング設定
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'logger': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+        'account': {
+            'handler': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+    'handler': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev'
+        },
+    },
+    'formatter': {
+        'dev': {
+            'format':
+            '\t'.join([
+                '%(astime)s', '[%(levelname)s]',
+                '%(pathneme)s(Line:%(lineno)d)', '%(message)s'
+            ])
+        },
+    }
+}
+#-------------------
+EMAIL_BACKEND = 'djago.core.mail.backends.console.EmailBackend'
