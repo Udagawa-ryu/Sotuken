6219a91898a0ca93c15a7d73cf20232b9aee4881
sugahara-yuuma <93749765+sugahara-yuuma@users.noreply.github.com>
2022-11-28 13:58:42 +0900

    a

--- maps/models.py
+++ maps/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from store.models import MO2_store
-from accounts.models import CustomUser,MO6_Visit_record,MO8_Fav_Search_condition
+from accounts.models import CustomUser
 
 # Create your models here.
 # タグモデル
