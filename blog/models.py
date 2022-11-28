aee941fe543df64ed421a18d56314500cb515c95
s_matsuoka <tky2002107@stu.o-hara.ac.jp>
2022-11-28 13:48:29 +0900

    modelsのMeta変更

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,19 @@ class MO7_Blog(models.Model):
   MO7_createDate= models.DateTimeField(verbose_name="作成日時", auto_now_add=True)
   MO7_updateDate= models.DateTimeField(verbose_name="更新日時", auto_now_add=True)
 
+  class Meta:
+    verbose_name_plural = "MO7_Blog"
+
+  def __str__(self):
+    return self.MO7_blogNumber
+
 #お気に入りブログモデル
 class MO10_Fav_Blog(models.Model):
   MO1_userNumber = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name="ユーザナンバー")
   MO7_blogNumber = models.ForeignKey(MO7_Blog, on_delete=models.CASCADE, verbose_name="ブログナンバー")
+
+  class Meta:
+    verbose_name_plural = "MO10_Fav_Blog"
+
+  def __str__(self):
+    return self.MO1_userNumber
--- maps/models.py
+++ maps/models.py
@@ -1,18 +1,28 @@
 from django.db import models
 from store.models import MO2_store
-from accounts.models import CustomUser
+from accounts.models import *
 
 # Create your models here.
 # タグモデル
 class MO5_Tag(models.Model):
   MO5_tagNumber = models.AutoField(verbose_name="タグナンバー",primary_key=True)
   MO5_tagName = models.CharField(verbose_name="タグ名")
-# デフォルトスポットモデル
+  class Meta:
+    verbose_name_plural = "MO5_Tag"
 
+  def __str__(self):
+    return self.MO5_tagNumber
+
+# デフォルトスポットモデル
 class MO3_Default_spot(models.Modele):
-  MO3_DsoptNumber = models.AutoField(verbose_name="デフォルトスポットナンバー", primary_key=True)
-  MO2_storeNumber = models.ForeignKey(MO2_store, models.CASCADE, verbose_name="ストアナンバー")
+  MO3_DspotNumber = models.AutoField(verbose_name="デフォルトスポットナンバー", primary_key=True)
+  MO2_storeNumber = models.ForeignKey(MO2_store, models.CASCADE, verbose_name="ストアナンバー", editable=False)
   MO5_tagNumber = models.ForeignKey(MO5_Tag,models.CASCADE, verbose_name="タグナンバー")
+  class Meta:
+    verbose_name_plural = "MO3_Default_spot"
+
+  def __str__(self):
+    return self.MO3_DspotNumber
 
 # オリジナルスポットモデル
 class MO4_Original_spot(models.Model):
@@ -23,9 +33,18 @@ class MO4_Original_spot(models.Model):
   MO4_createDate = models.DateTimeField(verbose_name="作成日時", auto_now_add=True)
   MO5_tagNumber = models.ForeignKey(MO5_Tag,models.CASCADE,verbose_name="タグナンバー" )
   MO1_userNumber = models.ForeignKey(CustomUser, models.CASCADE,verbose_name="ユーザナンバー")
+  class Meta:
+    verbose_name_plural = "MO4_Original_spot"
 
+  def __str__(self):
+    return self.MO4_OspotNumber
 
 # お気に入り検索条件モデル
 class MO8_Fav_Search_condition(models.Model):
   MO1_userNumber = models.ForeignKey(CustomUser, models.CASCADE, verbose_name="ユーザナンバー")
   MO5_tagNumber = models.ForeignKey(MO5_Tag,models.CASCADE, verbose_name="タグナンバー")
+  class Meta:
+    verbose_name_plural = "MO8_Fav_Search_condition"
+
+  def __str__(self):
+    return self.MO1_userNumber
