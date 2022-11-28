a4a6b443d838fe40917dbbc20968b859060ab692
s_matsuoka <tky2002107@stu.o-hara.ac.jp>
2022-11-28 14:20:14 +0900

    blog

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from accounts.models import *
+# from accounts.models import *
 # Create your models here.
 
 # ブログモデル
@@ -7,8 +7,8 @@ class MO7_Blog(models.Model):
   MO7_blogNumber = models.AutoField(verbose_name="ブログナンバー",primary_key=True,editable=False)
   MO7_blogName = models.CharField(verbose_name="ブログ名", max_length=50, blank=True)
   MO7_blogText = models.TextField(verbose_name="ブログ内容", null=True)
-  MO1_userID = models.ForeignKey(CustomUser,on_delete=models.CASCADE, verbose_name="ユーザID")
-  MO6_visitRecordNumber= models.ForeignKey(MO6_Visit_record, on_delete=models.CASCADE, verbose_name="訪問記録ナンバー")
+  MO1_userID = models.ForeignKey("accounts.CustomUser", models.CASCADE, verbose_name="ユーザID")
+  MO6_visitRecordNumber= models.ForeignKey("accounts.MO6_Visit_record", models.CASCADE, verbose_name="訪問記録ナンバー")
   MO7_openRange= models.IntegerField(verbose_name="公開範囲", default=0)
   MO7_createDate= models.DateTimeField(verbose_name="作成日時", auto_now_add=True)
   MO7_updateDate= models.DateTimeField(verbose_name="更新日時", auto_now_add=True)
@@ -21,8 +21,8 @@ class MO7_Blog(models.Model):
 
 #お気に入りブログモデル
 class MO10_Fav_Blog(models.Model):
-  MO1_userNumber = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name="ユーザナンバー")
-  MO7_blogNumber = models.ForeignKey(MO7_Blog, on_delete=models.CASCADE, verbose_name="ブログナンバー")
+  MO1_userNumber = models.ForeignKey("accounts.CustomUser", models.CASCADE, verbose_name="ユーザナンバー")
+  MO7_blogNumber = models.ForeignKey(MO7_Blog, models.CASCADE, verbose_name="ブログナンバー")
 
   class Meta:
     verbose_name_plural = "MO10_Fav_Blog"
