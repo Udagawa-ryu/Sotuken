9f9c883dd3633e776d132030c0a014c41af46416
sugahara-yuuma <93749765+sugahara-yuuma@users.noreply.github.com>
2022-11-29 14:59:24 +0900

    blog

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,14 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
+    path('blogList', views.BlogListView.as_view(), name="blogList"),
+    path('blogRegister', views.BlogRegisterView.as_view(), name="blogRegister"),
+    path('blogConfirmation', views.BlogConfirmationView.as_view(), name="blogConfirmation"),
+    path('blogCompletion', views.BlogCompletionView.as_view(), name="blogCompletion"),
+    path('blogDetail', views.BlogDetailView.as_view(), name="blogDetail"),
+    path('blogEdit', views.BlogEditView.as_view(), name="blogEdit"),
+    path('blogDelete', views.BlogDeleteView.as_view(), name="blogDelete"),
+    path('openRangeRegister', views.OpenRangeRegisterView.as_view(), name="openRangeRegister"),
+    path('otherBlogList', views.OtherBlogListView.as_view(), name="otherBlogList"),
+    path('favoriteBlogList', views.FavoriteBlogListView.as_view(), name="favoriteBlogList"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,33 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name= "BlogList.html"
+
+class BlogListView(generic.View):
+    template_name = "BlogList.html"
+
+class BlogRegisterView(generic.View):
+    template_name = "BlogRegister.html"
+
+class BlogConfirmationView(generic.View):
+    template_name = "BlogConfirmation.html"
+
+class BlogCompletionView(generic.View):
+    template_name = "BlogCompletion.html"
+
+class BlogDetailView(generic.View):
+    template_name = "BlogDetail.html"
+
+class BlogEditView(generic.View):
+    template_name = "BlogEdit.html"
+
+class BlogDeleteView(generic.View):
+    template_name = "BlogDelete.html"
+
+class OpenRangeRegisterView(generic.View):
+    template_name = "OpenRangeRegister.html"
+
+class OtherBlogListView(generic.View):
+    template_name = "OtherBlogList.html"
+
+class FavoriteBlogListView(generic.View):
+    template_name = "FavoriteBlogList.html"
