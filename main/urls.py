810cad413b32eb827df27f6bedf73d9fe253fd23
s_matsuoka <tky2002107@stu.o-hara.ac.jp>
2022-11-29 12:17:46 +0900

    samuraiwalk,urls,views

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,12 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     path('', views.MypageView.as_view(), name="Mypage"),
+    path('point',views.PointView.as_view(), name="Point"),
+
+
+    # path('',views.FavoriteView.as_view(), name="Favorite"),
+    # path('',views.BlogView.as_view(), name="Blog"),
+    # path('',views.MapView.as_view(), name="Map"),
+    # path('',views.PublicScopeView.as_view(), name="PublicScope"),
+    # path('',views.SearchView.as_view(), name="Search"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,24 @@ from django.views import generic
 
 class MypageView(generic.TemplateView):
     template_name= "Mypage.html"
+class PointView(generic.TemplateView):
+    template_name = "Point.html"
+
+
+
+
+# class FavoriteView(generic.TemplateView):
+#     template_name = "Favorite.html"
+
+
+# class BlogView(generic.TemplateView):
+#     template_name = "Blog.html"
+
+# class MapView(generic.TemplateView):
+#     template_name = "Map.html"
+
+# class PublicScopeView(generic.TemplateView):
+#     template_name = "Point.html"
+
+# class SearchView(generic.TemplateView):
+#     template_name = "Point.html"
