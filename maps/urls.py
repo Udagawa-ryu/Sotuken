d8b8b63fdbcdf2bc988915b6122d956eed3b7fef
s_matsuoka <tky2002107@stu.o-hara.ac.jp>
2022-11-30 09:37:31 +0900

    Map

--- maps/urls.py
+++ maps/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'maps'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name="Map"),
+    path('', views.MapView.as_view(), name="Map"),
     path('OspotInfo/', views.OspotInfoView.as_view(), name="OspotInfo"),
     path('OspotInfo/', views.OspotVisitRegisterView.as_view(), name="OspotVisitRegister"),
     path('OspotInfo/', views.DspotInfoView.as_view(), name="DspotInfo"),
