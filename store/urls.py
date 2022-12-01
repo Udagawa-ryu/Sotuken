b92e9e1e657907954dfacaaa09f761a58e65d832
Udagawa-ryu <93694931+Udagawa-ryu@users.noreply.github.com>
2022-12-01 11:14:13 +0900

    karioki

--- store/urls.py
+++ store/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('addStore/',views.addStore,name="addStore"),
     path('storePassRegister/<str:store>/',views.StorePassRegister,name="storePassRegister"),
     path('storeLogin/',views.storeLogin,name="storeLogin"),
+    path('karioki/',views.IndexView2,name="StoreMypage2"),
 ]
