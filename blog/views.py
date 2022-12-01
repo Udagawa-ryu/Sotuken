c6a09fd197121059f5bf42e7dedf2c80c36c82a8
s_matsuoka <tky2002107@stu.o-hara.ac.jp>
2022-12-01 15:10:47 +0900

    blog

--- blog/views.py
+++ blog/views.py
@@ -1,49 +1,78 @@
 from django.shortcuts import render,redirect
 from django.views import generic
-from .forms import *
-from accounts.models import CustomUser
+from .forms import BlogRegisterForm
+from accounts.models import CustomUser,MO6_Visit_record
+from django.urls import reverse_lazy
 # Create your views here.
 
 # ブログ一覧画面
-class BlogListView(generic.FormView):
+class BlogListView(generic.TemplateView):
     template_name = "BlogList.html"
 
 # ブログ新規作成画面
-# class BlogRegisterView(generic.View):
-#     template_name = "BlogRegister.html"
-#     form_class = BlogRegisterForm
-def BlogRegisterView(request):
-    CHOICE = {
-        (0,'OPEN'),
-        (1,'HIDDEN'),
-    }
-    params = {"message":'初期メッセージ',"form":None}
-    user = CustomUser.objects.get(MO1_userNumber=request.user.MO1_userNumber)
-    form = BlogRegisterForm()
-    my_record = MO6_Visit_record.objects.filter(MO1_userNumber=user)
-    form.fields['MO6_visitRecordNumber'].queryset = my_record
-    form.fields['MO7_openRange'].choices = CHOICE
-    if request.method == 'POST':
-        post_form = BlogRegisterForm(data=request.POST)
-        post_form.fields['MO6_visitRecordNumber'].queryset = my_record
-        post_form.fields['MO7_openRange'].choices = CHOICE
-        if post_form.is_valid():
-            params['form'] = post_form
-            params['message'] = "Please check your entries"
-            if request.POST.get('next', '') == 'confirm':
-                return render(request,"BlogConfirmation.html",params)
-            if request.POST.get('next', '') == 'back':
-                return render(request,"BlogRegister.html",params)
-            if request.POST.get('next', '') == 'create':
-                post_form.save()
-                return redirect("BlogList.html")
+class BlogRegisterView(generic.CreateView):
+    template_name = "BlogRegister.html"
+    form_class = BlogRegisterForm
+    model = MO6_Visit_record
+    success_url = reverse_lazy('blog:blogList')
+    def get_form(self,form_class=None):
+        form = super().get_form(form_class=form_class)
+        CHOICE = {
+            (0,'OPEN'),
+            (1,'HIDDEN'),
+        }
+        user = CustomUser.objects.get(MO1_userNumber=self.request.user.MO1_userNumber)
+        my_record = MO6_Visit_record.objects.filter(MO1_userNumber=user)
+        form.fields['MO6_visitRecordNumber'].queryset = my_record
+        form.fields['MO7_openRange'].choices = CHOICE
+        return form
+
+    def form_valid(self, form):
+        params = {'message':'','form': form}
+        if self.request.POST.get('next', '') == 'confirm':
+            return render(self.request,"BlogConfirmation.html",params)
+        if self.request.POST.get('next', '') == 'back':
+            return render(self.request,"BlogRegister.html",params)
+        if self.request.POST.get('next', '') == 'create':
+            form.save()
+            return redirect("BlogList.html")
         else:
-            params['form'] = form
-            params['message'] = '再入力して下さい'
-            return render(request, 'BlogRegister.html', params)
-    else :
-        params['form'] = form
-        return render(request,"BlogRegister.html",params)
+            # 正常動作ではここは通らない。エラーページへの遷移でも良い
+            return redirect(reverse_lazy('base:top'))
+# def BlogRegisterView(request):
+#     CHOICE = {
+#         (0,'OPEN'),
+#         (1,'HIDDEN'),
+#     }
+#     params = {"message":'初期メッセージ',"form":None}
+#     user = CustomUser.objects.get(MO1_userNumber=request.user.MO1_userNumber)
+#     my_record = MO6_Visit_record.objects.filter(MO1_userNumber=user)
+#     if request.method == 'POST':
+#         print(request.POST)
+#         post_form = BlogRegisterForm(request.POST)
+#         post_form.fields['MO6_visitRecordNumber'].queryset = my_record
+#         post_form.fields['MO7_openRange'].choices = CHOICE
+#         if post_form.is_valid():
+#             params['form'] = post_form
+#             params['message'] = "Please check your entries"
+#             if request.POST.get('next', '') == 'confirm':
+#                 return render(request,"BlogConfirmation.html",params)
+#             if request.POST.get('next', '') == 'back':
+#                 return render(request,"BlogRegister.html",params)
+#             if request.POST.get('next', '') == 'create':
+#                 post_form.save()
+#                 return redirect("BlogList.html")
+#         else:
+#             params['form'] = post_form
+#             print(post_form)
+#             params['message'] = '再入力して下さい'
+#             return render(request, 'BlogRegister.html', params)
+#     else :
+#         form = BlogRegisterForm()
+#         form.fields['MO6_visitRecordNumber'].queryset = my_record
+#         form.fields['MO7_openRange'].choices = CHOICE
+#         params['form'] = form
+#         return render(request,"BlogRegister.html",params)
     
 
 
