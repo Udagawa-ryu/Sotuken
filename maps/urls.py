ca7b7614fff7c06673e1fa738e1529f34e9e98e1
ryota0607 <tky2002022@stu.o-hara.ac.jp>
2022-12-02 14:40:15 +0900

    オリジナルスポット訪問記録登録画面

--- maps/urls.py
+++ maps/urls.py
@@ -5,7 +5,7 @@ app_name = 'maps'
 urlpatterns = [
     path('', views.MapView.as_view(), name="Map"),
     path('OspotInfo/', views.OspotInfoView.as_view(), name="OspotInfo"),
-    path('OspotInfo/', views.OspotVisitRegisterView.as_view(), name="OspotVisitRegister"),
+    path('OspotVisitRegister/', views.OspotVisitRegisterView.as_view(), name="OspotVisitRegister"),
     path('OspotInfo/', views.DspotInfoView.as_view(), name="DspotInfo"),
     path('OspotInfo/', views.OtherMapView.as_view(), name="OtherMap"),
 ]
