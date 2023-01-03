534c6e67a136a1ac15c8e0e9c8fefed9ab1fd0ed
ryota0607 <tky2002022@stu.o-hara.ac.jp>
2023-01-03 19:07:57 +0900

    デフォルトスポット画面

--- maps/urls.py
+++ maps/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('',views.Map,name="Map"),
     path('OspotInfo/', views.OspotInfoView.as_view(), name="OspotInfo"),
     path('OspotVisitRegister/', views.OspotVisitRegisterView.as_view(), name="OspotVisitRegister"),
-    path('OspotInfo/', views.DspotInfoView.as_view(), name="DspotInfo"),
+    path('DspotInfo/', views.DspotInfoView.as_view(), name="DspotInfo"),
     path('OspotInfo/', views.OtherMapView.as_view(), name="OtherMap"),
 ]
