4c0c8f50b9b5a2ab06b6058a63489d6e4a5d1b55
Udagawa-ryu <93694931+Udagawa-ryu@users.noreply.github.com>
2023-02-02 09:27:09 +0900

    OspotName

--- maps/forms.py
+++ maps/forms.py
@@ -3,9 +3,15 @@ from accounts.models import *
 from django import forms
 
 class OspotCreateForm(forms.ModelForm):
+    MO4_OspotName = forms.CharField(max_length=50,required=True)
     class Meta:
         model = MO4_Original_spot
         fields = ('MO1_userNumber','MO4_OspotName','MO4_OspotInfo','MO4_OspotLat','MO4_OspotLng')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
 
 class SpotSearchForm(forms.Form):
     words = forms.CharField(label="keyword",max_length=40)
